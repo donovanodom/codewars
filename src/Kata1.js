739470800937db2c295b6ce136c33ad1a2941be4
donovanodom <divdonis@gmail.com>
2022-02-18 12:41:42 -0800

    Added Find the next perfect square

--- src/Kata1.js
+++ src/Kata1.js
@@ -305,6 +305,46 @@ export const TheHashtagGenerator = () => {
   );
 };
 
+export const FindTheNextPerfectSquare = () => {
+  const code = `function findNextSquare(sq) {
+  if (Math.sqrt(sq) % 1 === 0){
+    const root = (Math.sqrt(sq) + 1)
+    return root * root
+  }else{
+    return -1
+  }
+}`;
+  return (
+    <>
+      <h2 className="title">Find the next perfect square!</h2>
+      <div className="description">
+        <h3>Description:</h3>
+        You might know some pretty large perfect squares. But what about the
+        NEXT one?
+        <br />
+        Complete the findNextSquare method that finds the next integral perfect
+        square after the one passed as a parameter. Recall that an integral
+        perfect square is an integer n such that sqrt(n) is also an integer.
+        <br />
+        If the parameter is itself not a perfect square then -1 should be
+        returned. You may assume the parameter is non-negative.
+        <br />
+        Examples:(Input --{`>`} Output)
+        <br />
+        121 --{`>`} 144
+        <br />
+        625 --{`>`} 676
+        <br />
+        114 --{`>`} -1 since 114 is not a perfect square
+      </div>
+      <h3>JavaScript</h3>
+      <SyntaxHighlighter language={"javascript"} style={darcula}>
+        {code}
+      </SyntaxHighlighter>
+    </>
+  );
+};
+
 const Kata1 = () => {
   return (
     <>
@@ -315,6 +355,7 @@ const Kata1 = () => {
       <SumStringsasNumbers />
       <SplitStrings />
       <TheHashtagGenerator />
+      <FindTheNextPerfectSquare />
     </>
   );
 };

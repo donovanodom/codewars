b2d1cf0a01f999df43838bf7cd988609f0685582
donovanodom <divdonis@gmail.com>
2022-02-18 13:00:25 -0800

    Added Find the Divisors

--- src/Kata1.js
+++ src/Kata1.js
@@ -377,6 +377,51 @@ export const RegexValidatePINCode = () => {
   );
 };
 
+export const FindTheDivisors = () => {
+  const code = `function divisors(integer) {
+  function isPrime(num) {
+    for (var i = 2; i < num; i++)
+      if (num % i === 0) return false;
+    return num > 1
+  }
+  if (isPrime(integer) === false) {
+    let factors = []
+    for (let i = 2; i < integer; i++) {
+      if (integer % i == 0) {
+        factors.push(i);
+      }
+    }
+    return factors
+  } else {
+    return \`\${integer} is prime\`\     
+  }
+}`;
+  return (
+    <>
+      <h2 className="title">Find the Divisors!</h2>
+      <div className="description">
+        <h3>Description:</h3>
+        Create a function named divisors/Divisors that takes an integer n > 1
+        and returns an array with all of the integer's divisors(except for 1 and
+        the number itself), from smallest to largest. If the number is prime
+        return the string '(integer) is prime'.
+        <br />
+        Example:
+        <br />
+        divisors(12); // should return [2,3,4,6]
+        <br />
+        divisors(25); // should return [5]
+        <br />
+        divisors(13); // should return "13 is prime"
+      </div>
+      <h3>JavaScript</h3>
+      <SyntaxHighlighter language={"javascript"} style={darcula}>
+        {code}
+      </SyntaxHighlighter>
+    </>
+  );
+};
+
 const Kata1 = () => {
   return (
     <>
@@ -389,6 +434,7 @@ const Kata1 = () => {
       <TheHashtagGenerator />
       <FindTheNextPerfectSquare />
       <RegexValidatePINCode />
+      <FindTheDivisors />
     </>
   );
 };
